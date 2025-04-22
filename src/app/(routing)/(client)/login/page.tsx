3d95f99bf37d354d042b0e0feb8b275ebb01864f
shuhrat <shyhratyagnobi2002@gmail>
2025-04-22 18:30:00 +0300

    build: настроен маршрут /login для продакшена

---
+++ src/app/(routing)/(client)/login/page.tsx
@@ -0,0 +1,9 @@
+import { TypographyH2 } from "@/shared/components/typografy/typographyH2";
+
+export default function Login(){
+    return <>
+    <TypographyH2 className="">
+        Page Login 
+    </TypographyH2>
+    </>
+}
