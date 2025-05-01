d1f09bf9db43bfba6ddffc016db2d939710091be
shuhrat <shyhratyagnobi2002@gmail>
2025-05-01 13:22:25 +0300

    refactor:удалил не нужние импорты

--- src/app/(routing)/(client)/(home)/page.tsx
+++ src/app/(routing)/(client)/(home)/page.tsx
@@ -1,21 +1,3 @@
-// 'use client'
-// import { CarouselSize } from "@/widgets/homeCarousel";
-// import Image from "next/image";
-
-// export default function Home() {
-//   return <section className="home w-[100%]  ">
-
-//     <div className=" relative    h-[530px] ">
-//       <Image className="object-cover"  fill src={"/firefighters-protective-gear-fight-blazing-inferno-generated-by-ai.jpg"} alt="background" />
-//       <CarouselSize />
-//     </div>
-
-
-
-
-//   </section>
-// }
-
 'use client'
 import { CarouselSize } from "@/widgets/homeCarousel";
 import Image from "next/image";
